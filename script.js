3a535d680a8801bd040882143dec0d6e787f9acb
solfizz <solfizz@gmail.com>
2015-09-17 19:21:53 -0700

    This is my second commit.

---
+++ script.js
@@ -0,0 +1,25 @@
+//STEP 1
+/*
+varSomeMonth
+functionTheMonth
+currentMonth
+varSummerMonth
+myLibraryAFunction
+*/
+
+//STEP 2
+/*
+numeric literal expression: 
+var start = 10;
+var increase = start ++;
+
+string literal expression:
+var favFood = "pizza";
+var argument = "But potatoes is better than " + favFood +".";
+
+boolean literal expression: 
+var start = 10;
+
+
+null literal expression:  
+*/
